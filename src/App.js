f7fdf2c295c7dbcbf09562bc0bb1a850f15e3dae
Kinjallimbasiya <kinjaldlimbasiya4@gmail.com>
2024-02-14 00:07:44 -0800

    first commite

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import "./style/index.scss";
+
+function App() {
+  return (
+    <div>
+      hello wolrd
+      <div className='button'>
+        <button className='button solid white' >Show me more</button>
+      </div>
+
+    </div>
+
+
+  );
+}
+
+export default App;
